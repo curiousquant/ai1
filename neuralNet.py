fa3546acf3fe0a57d11a07a60d0d58421d7b7b1b
curiousquant <56905431+curiousquant@users.noreply.github.com>
2020-04-21 19:55:39 -0400

    add neural net test

---
+++ neuralNet.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class neuralNet():
+    
+    def __init__(self,X,N):
+        self.X = X
+        self.N = N
+        self.theta = np.zeros((N,self.X.shape[0]))
+
+    def actFunc(self):
+        
+
+
+
+
+if __name__ = '__main__':
